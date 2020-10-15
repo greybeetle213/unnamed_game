df9b3396a6bb9f0a4fe68e148201103464d1c79f
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2020-10-16 11:06:43 +1300

    Add files via upload

--- keypresses.js
+++ keypresses.js
@@ -5,72 +5,113 @@ var up = false
 var enter = false
 var keyX = false
 
-function touchEnter(){
-    enter = true
+function touchMode(){
+    var counter = 0
+    for (counter = 0; counter < 6; counter ++){
+        document.getElementById("touch"+counter).style.visibility = "visible"
+    }
 }
-document.getElementById("Enter").onmouseup = "touchEnter()"
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    alert("mobile")
-}else{
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'ArrowLeft') {
-            left = true
-        }
-    })
-    document.addEventListener('keyup', (event) => {
-        if (event.key == 'ArrowLeft') {
-            left = false
-        }
-    })
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'ArrowRight') {
-            right = true
-        }
-    })
-    document.addEventListener('keyup', (event) => {
-        if (event.key == 'ArrowRight') {
-            right = false
-        }
-    })
+function touchEnter(event){
+    if (event == 'down'){
+        enter = true
+    }else{
+        enter = false
+    }
+}
+function touchA(event){
+    if (event == 'down'){
+        keyX = true
+    }else{
+        keyX = false
+    }
+}
+function touchUp(event){
+    if (event == 'down'){
+        up = true
+    }else{
+        up = false
+    }
+}
+function touchDown(event){
+    if (event == 'down'){
+        down = true
+    }else{
+        down = false
+    }
+}
+function touchLeft(event){
+    if (event == 'down'){
+        left = true
+    }else{
+        left = false
+    }
+}
+function touchRight(event){
+    if (event == 'down'){
+        right = true
+    }else{
+        right = false
+    }
+}
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowLeft') {
+        left = true
+    }
+})
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'ArrowLeft') {
+        left = false
+    }
+})
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight') {
+        right = true
+    }
+})
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'ArrowRight') {
+        right = false
+    }
+})
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'ArrowUp') {
-            up = true
-        }
-    })
-    document.addEventListener('keyup', (event) => {
-        if (event.key == 'ArrowUp') {
-            up = false
-        }
-    })
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'ArrowDown') {
-            down = true
-        }
-    })
-    document.addEventListener('keyup', (event) => {
-        if (event.key == 'ArrowDown') {
-            down = false
-        }
-    })
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'x') {
-            keyX = true
-        }
-    })
-    document.addEventListener('keyup', (event) => {
-        if (event.key == 'x') {
-            keyX = false
-        }
-    })
-    document.addEventListener('keydown', (event) => {
-        if (event.key == 'Enter') {
-            enter = true
-        }
-    })
-    document.addEventListener('keyup', (event) => {
-        if (event.key == 'Enter') {
-            enter = false
-        }
-    })
-}
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowUp') {
+        up = true
+    }
+})
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'ArrowUp') {
+        up = false
+    }
+})
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowDown') {
+        down = true
+    }
+})
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'ArrowDown') {
+        down = false
+    }
+})
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'x') {
+        keyX = true
+    }
+})
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'x') {
+        keyX = false
+    }
+})
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') {
+        enter = true
+    }
+})
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'Enter') {
+        enter = false
+    }
+})
+
